dffc31c87e443569355414791f0c83a37b8a5235
chamuzxmd-v2 <chamuzxmd@gmail.com>
2026-02-18 14:15:36 +0530

    Add petAbility function to enhance coin collection

--- pet.js
+++ pet.js
@@ -9,3 +9,8 @@ function petAbility() {
   player.coins += lootFound;
   console.log(`${pet.name} found ${lootFound} coins! Total coins: ${player.coins}`);
 }
+function petAbility() {
+  let loot = Math.floor(Math.random()*100) + 50;
+  player.coins += loot;
+  console.log("Pet found coins:", loot, "Total coins:", player.coins);
+}
