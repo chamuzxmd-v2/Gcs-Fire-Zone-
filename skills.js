b458f0d84de4aeca472a4315acf62190965f9c86
chamuzxmd-v2 <chamuzxmd@gmail.com>
2026-02-18 14:14:57 +0530

    Add new skills: healAura, speedBoost, and shield

--- skills.js
+++ skills.js
@@ -15,3 +15,19 @@ let skills = {
     console.log("Shield activated! Shield:", player.shield);
   }
 };
+let skills = {
+  healAura: () => { 
+    player.hp += 5; 
+    if(player.hp > 100) player.hp = 100; 
+    console.log("Heal Aura! HP:", player.hp);
+  },
+  speedBoost: () => {
+    player.speed += 5;
+    setTimeout(()=>{ player.speed -= 5; }, 5000);
+    console.log("Speed Boost! Speed:", player.speed);
+  },
+  shield: () => {
+    player.shield += 10;
+    console.log("Shield! Shield Points:", player.shield);
+  }
+};
